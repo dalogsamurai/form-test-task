b6d079beca7343d1dec0406c24e6f625a071a198
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-11-24 00:40:57 +0300

    wip

---
+++ src/components/HeaderMenu/header-menu.tsx
@@ -0,0 +1,19 @@
+import iArrow from "../../assets/arrow.svg";
+import iMenu from "../../assets/menu.svg";
+import "./header-menu.sass";
+
+export const HeaderMenu = () => {
+  //не очень понял идею с barrel export, поэтому так
+  return (
+    <div className="header-menu">
+      <div className="header-menu__item">
+        <img src={iMenu} alt="" className="header-menu__img" />
+      </div>
+      <div className="header-menu__item">
+        <img src={iArrow} alt="" className="header-menu__img" />
+      </div>
+      <div className="header-menu__item"></div>
+      <div className="header-menu__item"></div>
+    </div>
+  );
+};
--- src/pages/main.page.tsx
+++ src/pages/main.page.tsx
@@ -1,7 +1,12 @@
+import { HeaderMenu } from "../components/HeaderMenu";
 import "./main.page.sass";
 
 const MainPage = () => {
-  return <div className="main-page"></div>;
+  return (
+    <div className="main-page">
+      <HeaderMenu />
+    </div>
+  );
 };
 
 export default MainPage;
