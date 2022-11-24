4da154fc02f44cabe43b76637c3be05a9c1013da
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-11-24 22:56:03 +0300

    menu-item

--- src/components/MenuItem/menu-item.tsx
+++ src/components/MenuItem/menu-item.tsx
@@ -1,5 +1,15 @@
+import { FC } from "react";
+import iMenuItem from "../../assets/menu-item-img.svg";
 import "./menu-item.sass";
 
-export const MenuItem = () => {
-  return <div className="menu-item"></div>;
+type Props = {
+  title: string;
+};
+export const MenuItem: FC<Props> = ({ title }) => {
+  return (
+    <div className="menu-item">
+      <img src={iMenuItem} alt="" className="menu-item__img" />
+      <div className="menu-item__text">{title}</div>
+    </div>
+  );
 };
