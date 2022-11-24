ddf94349d9a759bf7e9c8151e54c12be5a829c4a
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-11-24 22:40:38 +0300

    upper menu done

---
+++ src/components/FolderTitle/folder-title.tsx
@@ -0,0 +1,9 @@
+import "./folder-title.sass";
+
+export const FolderTitle = () => {
+  return (
+    <div className="folder-title">
+      <div className="folder-title__text">Строительно-монтажные работы</div>
+    </div>
+  );
+};
--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,9 +1,14 @@
+import { FolderTitle } from "../FolderTitle/folder-title";
 import { UpperSection } from "../UpperSection/upper-section";
+import "./menu.sass";
 
 export const Menu = () => {
   return (
     <div className="menu">
-      <UpperSection />
+      <div className="menu__upper">
+        <UpperSection />
+        <FolderTitle />
+      </div>
     </div>
   );
 };
