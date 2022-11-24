afe95302079600d0620117de91c3d93d11b309e0
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-11-24 23:53:28 +0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/main.page";
+import { Footer } from "./components/Footer/footer";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/components/Footer/footer.tsx
@@ -0,0 +1,5 @@
+import "./footer.sass";
+
+export const Footer = () => {
+  return <div className="footer"></div>;
+};
